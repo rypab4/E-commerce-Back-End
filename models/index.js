d68d840cc1ab9ae6542fb62c05b18ff56b3950be
pabfam <pabalate@Pabalates-MacBook-Pro.local>
2023-01-31 17:34:41 -0800

    add given files

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id,'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+}
+  )
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
